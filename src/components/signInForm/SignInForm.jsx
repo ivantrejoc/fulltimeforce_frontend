ddf09bb18be620c128cc0fc8dc874de7a60b83e5
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2024-08-05 12:49:41 -0500

    signin form responsive

--- src/components/signInForm/SignInForm.jsx
+++ src/components/signInForm/SignInForm.jsx
@@ -2,26 +2,26 @@ import "./signInForm.scss";
 
 const SignInForm = () => {
   return (
-    <div class="signin">
-    <div class="content">
+    <div className="signin">
+    <div className="content">
     <div className="logo-wrapper">
       <h2 className="logo">Welcome to My Blog</h2>
       </div>
       <div className="title-wrapper">
       <h2>Sign in</h2>
       </div>
-      <form class="form">
-        <div class="inputBox">
+      <form className="form">
+        <div className="inputBox">
           <label htmlFor="">Email</label>
           <input type="text" placeholder="Johndoe@mail.com" required /> 
           <p>errors....</p>
         </div>
-        <div class="inputBox">
+        <div className="inputBox">
           <label htmlFor="">Password</label>
-          <input type="text" placeholder="johndoe21" required /> 
+          <input type="password" placeholder="Your password" required /> 
           <p>errors....</p>
         </div>       
-        <div class="button-box">
+        <div className="button-box">
           <button className="button" type="submit">Sign in </button>
           <a className="link" href="/signup">You don't have an account yet? Click here</a>
         </div>
